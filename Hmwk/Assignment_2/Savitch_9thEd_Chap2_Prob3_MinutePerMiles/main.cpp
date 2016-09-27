cc7c88cffc00addf22c7145ba00c1674ed2c881f
rcc <rcc>
2016-09-27 12:42:17 -0700

    Signed-off-by: rcc <rcc>

---
+++ Hmwk/Assignment_2/Savitch_9thEd_Chap2_Prob3_MinutePerMiles/main.cpp
@@ -0,0 +1,30 @@
+/* 
+   File:   main
+   Author: Ricardo Bartolo Jr.
+   Created on September 27, 2016, 12:26 PM
+   Purpose:  "Speed in mph and converts the quantity into minutes and seconds per mile.
+ */
+
+//System Libraries
+#include <iostream>   //Input/Output objects
+using namespace std;  //Name-space used in the System Library
+
+//User Libraries
+
+//Global Constants
+
+//Function prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declaration of Variables
+    
+    //Input values
+    
+    //Process values -> Map inputs to Outputs
+    
+    //Display Output
+
+    //Exit Program
+    return 0;
+}
---
+++ Lab/TruthTable/main.cpp
@@ -0,0 +1,67 @@
+/* 
+   File:   main
+   Author: Ricardo Bartolo Jr.
+   Created on September 27th, 2016, 12:12 PM
+   Purpose:  Display 13 columns of the truth table
+ */
+
+//System Libraries
+#include <iostream>   //Input/Output objects
+using namespace std;  //Name-space used in the System Library
+
+//User Libraries
+
+//Global Constants
+
+//Function prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declaration of Variables
+    bool x,y;
+    
+    //Output the Heading of the truth table
+    cout<<"X Y !X !Y X&&Y X||Y X^Y X^Y^Y X^Y^X !(X&&Y) !X||!Y !(X||Y) !X&&!Y\n";
+    
+    //Input values for first row of the truth table
+    x=true;
+    y=true;
+    
+    //Display the first row of the truth table
+    cout<<(x?'T':'F')<<" ";
+    cout<<(y?'T':'F')<<"  ";
+    cout<<(!x?'T':'F')<<"  ";
+    cout<<(!y?'T':'F')<<"   ";
+    cout<<(x&&y?'T':'F')<<"    ";
+    cout<<(x||y?'T':'F')<<"   ";
+    cout<<(x^y?'T':'F')<<"    ";
+    cout<<(x^y^y?'T':'F')<<"     ";
+    cout<<(x^y^x?'T':'F')<<"       ";
+    cout<<(!(x&&y)?'T':'F')<<"      ";
+    cout<<(!x||!y?'T':'F')<<"      ";
+    cout<<(!(x||y)?'T':'F')<<"      ";
+    cout<<(!x&&!y?'T':'F')<<" ";
+    cout<<endl;
+    
+    //Input values for second row of the truth table
+    y=false;
+    
+    //Display the second row of the truth table
+    cout<<(x?'T':'F')<<" ";
+    cout<<(y?'T':'F')<<"  ";
+    cout<<(!x?'T':'F')<<"  ";
+    cout<<(!y?'T':'F')<<"   ";
+    cout<<(x&&y?'T':'F')<<"    ";
+    cout<<(x||y?'T':'F')<<"   ";
+    cout<<(x^y?'T':'F')<<"    ";
+    cout<<(x^y^y?'T':'F')<<"     ";
+    cout<<(x^y^x?'T':'F')<<"       ";
+    cout<<(!(x&&y)?'T':'F')<<"      ";
+    cout<<(!x||!y?'T':'F')<<"      ";
+    cout<<(!(x||y)?'T':'F')<<"      ";
+    cout<<(!x&&!y?'T':'F')<<" ";
+    cout<<endl;
+
+    //Exit Program
+    return 0;
+}
