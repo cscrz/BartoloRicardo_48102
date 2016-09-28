e4694c35a5e0c8875684e1561a7625a1c33811ce
rezzer33 <rezbot33@gmail.com>
2016-09-28 00:23:13 -0700

    Signed-off-by: rezzer33 <rezbot33@gmail.com>

--- Hmwk/Assignment_2/Savitch_9thEd_Chap2_Prob3_MinutePerMiles/main.cpp
+++ Hmwk/Assignment_2/Savitch_9thEd_Chap2_Prob3_MinutePerMiles/main.cpp
@@ -12,18 +12,38 @@ using namespace std;  //Name-space used in the System Library
 //User Libraries
 
 //Global Constants
+const unsigned short
+MINPH=60,               //Minutes Per Hour
+?????=1;                //IDK WHAT THIS THING IS
+
 
 //Function prototypes
 
 //Execution Begins Here!
 int main(int argc, char** argv) {
     //Declaration of Variables
+    unsigned short
+    spd;                //Speed (MPH) the user will input
+    
+    float
+    mpm,                //Miles per minute
+    minpm;              //Minute per miles
     
     //Input values
     
     //Process values -> Map inputs to Outputs
     
     //Display Output
+    cout<<"Please enter the speed (MPH)."<<endl;
+    cin>>spd;
+    
+    mpm=spd/MINPH;
+    minpm=?????/mpm;
+    
+    cout<<endl;
+    cout<<"You entered "<<spd<<" MPH."<<endl;
+    cout<<"That is equivalent to "<<minpm<<" minutes per mile."<<endl;
+    
 
     //Exit Program
     return 0;
