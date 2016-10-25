ab9d66269c2bc7a184ebcbd71028fcf566266b4f
rcc <rcc>
2016-10-25 12:33:50 -0700

    Signed-off-by: rcc <rcc>

--- Proj/Project 1/Project_1_WordGuess/main.cpp
+++ Proj/Project 1/Project_1_WordGuess/main.cpp
@@ -20,6 +20,8 @@ using namespace std;    //Name-space used in the System Library
 //Global Constants
 
 //Function prototypes
+void prog(int,int,string);
+int find(int,int,string);
 
 //Execution Begins Here!
 int main(int argc, char** argv) {
@@ -33,9 +35,40 @@ int main(int argc, char** argv) {
     
     string  name;
     
+    char    uan;
+    
     int     rn,
             size,
-            ch1;
+            uana,
+            ch1,
+            ch2,
+            ch3,
+            ch4,
+            ch5,
+            ch6,
+            ch7,
+            ch8,
+            ch9,
+            ch10,
+            ch11,
+            ch12,
+            ch13,
+            ch14,
+            a1,
+            a2,
+            a3,
+            a4,
+            a5,
+            a6,
+            a7,
+            a8,
+            a9,
+            a10,
+            a11,
+            a12,
+            a13,
+            a14,
+            fn;
     
     rn=rand()%15+1;
     
@@ -47,20 +80,64 @@ int main(int argc, char** argv) {
     }
     
     ch1=name[0];
+    ch2=name[1];
+    ch3=name[2];
+    ch4=name[3];
+    ch5=name[4];
+    ch6=name[5];
+    ch7=name[6];
+    ch8=name[7];
+    ch9=name[8];
+    ch10=name[9];
+    ch11=name[10];
+    ch12=name[11];
+    ch13=name[12];
+    ch14=name[13];
+    
     size=name.size();
     
     cout<<"Rand="<<rn<<endl;
-    cout<<name<<size<<ch1;
+    cout<<ch1<<" "<<ch2<<" "<<ch3<<" "<<ch4<<" "<<ch5<<" "<<ch6<<" "<<ch7<<" ";
+    cout<<ch8<<" "<<ch9<<" "<<ch10<<" "<<ch11<<" "<<ch12<<" "<<ch13<<" "<<ch14<<endl;
     
+    cout<<name<<size<<static_cast<char>(ch1)<<endl<<endl;
     
-    //Input values
-//    in.open("words.dat");
-//    in>>name1>>name2;
-//    cout<<"Name 1 = "<<name1<<endl;
-//    cout<<"Name 2 = "<<name2<<endl;
+    prog(uana,size,name);
     
+    cout<<"Guess a letter: ";cin>>uan;
+    
+    uana=static_cast<int>(uan);
+    
+    if(uana>60 && uana<123)                                 //If lower case
+    {
+        uana-=32;                                           //Make it UPPERCASE
+    }
+    
+    uan=static_cast<char>(uana);
+    cout<<static_cast<char>(uana)<<endl;
+    
+    switch(fn)
+    {
+        case 0: a1=1; break;
+        case 1: a2=1; break;
+        case 2: a3=1; break;
+        case 3: a4=1; break;
+        case 4: a5=1; break;
+        case 5: a6=1; break;
+        case 6: a7=1; break;
+        case 7: a8=1; break;
+        case 8: a9=1; break;
+        case 9: a10=1; break;
+        case 10: a11=1; break;
+        case 11: a12=1; break;
+        case 12: a13=1; break;
+        case 13: a14=1; break;
+        default: break;
+    }
 
+    prog(uana,size,name);
     
+    cout<<uana<<static_cast<int>(name[0]);
     
     
     //Process values -> Map inputs to Outputs
@@ -70,4 +147,34 @@ int main(int argc, char** argv) {
     ine.close();
     //Exit Program
     return 0;
+}
+
+void prog(int let, int size, string nm)
+{
+    for(int sz=size;sz>0;sz--)
+    {
+        int x;
+        if (let==static_cast<int>(nm[x]))
+        {
+            cout<<"!!!!";
+        }
+        else
+        {
+            cout<<"_ ";
+        }
+        x++;
+    }
+}
+
+int find(int let, int size, string nm)
+{
+    int x;
+    for(int sz=size;sz>0;sz--)
+    {
+        if(let==nm[x])
+        {
+            return x;
+        }
+        x++;
+    }
 }
